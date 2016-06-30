2a7b2e7ef00f765a47a185cb13217965dc95e412
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-06-30 18:22:41 +0300

    fixed redirect

--- src/main/java/com/epam/training/servlets/DataHandler.java
+++ src/main/java/com/epam/training/servlets/DataHandler.java
@@ -2,6 +2,7 @@ package com.epam.training.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,29 +17,6 @@ import javax.servlet.http.HttpSession;
 
 public class DataHandler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		HttpSession session = request.getSession(true);
-		List<String> sessionList = (List<String>) session.getAttribute(session.getId());
-		PrintWriter out = new PrintWriter(response.getWriter());
-		
-		if (sessionList == null) {
-			request.getRequestDispatcher("WEB-INF/error.jsp");
-		}
-		out.println("<html>");
-			out.println("<head>");
-				out.println("<title>Страница заказов</title>");
-			out.println("<body>");
-			out.println("Session id: " + session.getId() + "<br>");
-			for (int i = 0; i < sessionList.size(); ++i) {
-				out.println(sessionList.get(i) + "<br>");
-			}
-			out.println("</body>");
-		    out.println("</head>");
-		out.println("</html>");
-		out.close();
-	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
@@ -57,6 +35,7 @@ public class DataHandler extends HttpServlet {
 		
 		session.setAttribute(session.getId(), sessionList);
 		
-		doGet(request, response);
+		response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+		response.sendRedirect("server");
 	}
 }
